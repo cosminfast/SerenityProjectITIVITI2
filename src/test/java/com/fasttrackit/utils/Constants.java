ed89e35a295c6eee22c2f0ea6e46ef94e4ec4c3e
Cosmin <cosmin.ciocan@savethechildren.org>
2022-02-11 11:01:49 +0200

    updates to architecture and tests

--- src/test/java/com/fasttrackit/utils/Constants.java
+++ src/test/java/com/fasttrackit/utils/Constants.java
@@ -8,4 +8,21 @@ public class Constants {
 //    public static final String BASE_URL = "http://testfasttrackit.info/selenium-test/";
     public static final String BASE_URL = PropertyFileReader.getEnvConstantsItem("BASE_URL");
 
+
+
+
+
+
+//    ------------ Running tests from CMD -------------
+//    mvn test - runs all test methods, depends on maven configurations
+//    mvn clean test - same as above, removes target folder first
+//    mvn clean test -Denv=qa - runs tests on QA env
+//    mvn clean test -Dtest=AllCartTestsSuite - runs specific suite
+//    mvn serenity:aggregate - generates serenity reports
+
+
+
+
+
+
 }
